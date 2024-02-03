72097e3fddcf4cde2c5d441d6221503d0c7394fb
hyobin-yang <hyobin3026@naver.com>
2024-02-03 23:24:18 +0100

    실전자바-Section2

--- src/ref/MethodChange1.java
+++ src/ref/MethodChange1.java
@@ -4,7 +4,8 @@ public class MethodChange1 {
     public static void main(String[] args) {
         int a = 10;
         System.out.println("메서드 호출 전 a = " + a);
-        System.out.println("메서드 호출 후 a = " + a);
+        changePrimitive(a);
+        System.out.println("메서드 호출 후 a = " + a); // 값 변하지 않음, 기본형 변수는 값만 그대로 복사되기 때문
     }
 
     static void changePrimitive(int x) {
--- src/ref/MethodChange2.java
+++ src/ref/MethodChange2.java
@@ -7,7 +7,7 @@ public class MethodChange2 {
 
         System.out.println("메서드 호출 전 dataA.value = " + dataA.value);
         changeReference(dataA);
-        System.out.println("메서드 호출 후 dataA.value = " + dataA.value);
+        System.out.println("메서드 호출 후 dataA.value = " + dataA.value); // 값 변함, 참조형 변수는 참조값이 복사되기 때문
     }
 
     static void changeReference(Data dataX) {
